5cf3a665101516ce93ded7879f379be14d69947d
Esfahan <calito0131@gmail.com>
2021-03-05 01:16:33 +0900

    Added the env for production

--- amongus/config.py
+++ amongus/config.py
@@ -17,5 +17,24 @@ class DevelopmentConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
+class ProductionConfig:
 
-Config = DevelopmentConfig
+    # Flask
+    DEBUG = True
+
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}'.format(**{
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASSWORD'),
+        'host': os.environ.get('DB_HOST'),
+        'port': os.environ.get('DB_PORT'),
+        'dbname': os.environ.get('DB_NAME'),
+    })
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+
+
+if os.environ.get('FALSK_ENV') == 'production':
+    Config = ProductionConfig
+else:
+    Config = DevelopmentConfig
